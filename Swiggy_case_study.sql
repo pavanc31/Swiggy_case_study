c512297600292e8ff9c4d63ad40b9ebd90e5c134
Pavan S Chavalgi <132770606+pavanc31@users.noreply.github.com>
2023-05-09 12:19:24 +0530

    Update Swiggy_case_study.sql

--- Swiggy_case_study.sql
+++ Swiggy_case_study.sql
@@ -1,14 +1,18 @@
 # 1. Find the customer who never ordered
--- First Method
+-- First Method (Using Joins)
 
 SELECT name FROM users AS u
 JOIN orders AS o
 ON u.user_id = o.user_id
 WHERE o.user_id IS NULL;
 
--- Second Method
+-- Second Method ( Using Sub_Querry)
+
 SELECT name FROM users 
 WHERE user_id NOT IN (SELECT user_id FROM orders);
+
+-- BY exexcuting this query we will find the customers who never orderd on the swiggy.
+
 
 # 2. Find the average price of each dish
 
@@ -16,6 +20,9 @@ SELECT f_name,AVG(price) AS avg_price FROM food AS f
 JOIN menu AS m
 ON f.f_id = m.f_id
 GROUP BY f_name;
+
+-- BY exexcuting this query we will get the average price of the each dish.
+
 
 # 3. Find the top restaurnts in terms of number of orders for a given month (june)
 
@@ -27,6 +34,8 @@ GROUP BY r_name
 ORDER BY max_orders DESC
 LIMIT 1;
 
+-- BY exexcuting this query we will get the restaurant who got the top orders in the particular month and the restarant is KFC.
+
 # 4. Restaurants with monthly sales greater then 1000
 
 SELECT r_name, SUM(amount) AS revenue FROM restaurants AS r 
@@ -34,7 +43,9 @@ JOIN orders AS o
 ON r.r_id = o.r_id
 WHERE MONTHNAME(date) = 'july'
 GROUP BY r_name
-HAVING SM(amount) > 1000;
+HAVING SUM(amount) > 1000;
+
+-- By this we can conclude that 3 restaurants have the sales greater then 1000.
 
 # 5. Show all orders with order details for a particular customer in a paricular range
 
@@ -47,7 +58,9 @@ JOIN users AS u
 ON o.user_id = u.user_id
 JOIN restaurants AS r
 ON r.r_id = o.r_id
-WHERE name ='neha' AND date > '2022-06-10' AND date <'2022-07-10';
+WHERE name ='ankit' AND date > '2022-06-10' AND date <'2022-07-10';
+
+-- on executing this Query we can find the details of the particular customer in a certain range of date
 
 # 6. Find the restauraunts with max repeated customers
 
@@ -64,6 +77,8 @@ FROM
 		ORDER BY LOYAL_CUSTOMERS DESC 
         LIMIT 1;
         
+-- We can say KFC has a most repeated or loyal customers by this query
+        
 # 7. Month over month growth of swiggy
 
 SELECT month,((revenue - prev)/prev)*100 AS per_revenue FROM
@@ -74,5 +89,7 @@ SELECT monthname(date)AS month,sum(amount)AS revenue FROM orders
 GROUP BY month
 )
 SELECT month,revenue,LAG(revenue,1) OVER(ORDER BY revenue) AS prev FROM sales) AS table1
+
+-- This shows the month over month revenue of the swiggy
 
 
